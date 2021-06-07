651f428e857d6596469e95d99183cfc4638489a4
Eric Warren II <ericwarrenii@Erics-MacBook-Pro.local>
2021-06-07 16:25:16 -0400

    initia commit

--- koans/about_string_manipulation.py
+++ koans/about_string_manipulation.py
@@ -6,43 +6,54 @@ from runner.koan import *
 class AboutStringManipulation(Koan):
 
     def test_use_format_to_interpolate_variables(self):
+        # The format() method formats the specified value(s) and insert them inside the string's placeholder.
+        # The placeholder is defined using curly brackets: {}. Read more about the placeholders in the Placeholder section below.
+        # The placeholders can be identified using named indexes {price}, numbered indexes {0}, or even empty placeholders {}.
         value1 = 'one'
         value2 = 2
         string = "The values are {0} and {1}".format(value1, value2)
-        self.assertEqual(__, string)
+        self.assertEqual("The values are one and 2", string)
 
     def test_formatted_values_can_be_shown_in_any_order_or_be_repeated(self):
         value1 = 'doh'
         value2 = 'DOH'
         string = "The values are {1}, {0}, {0} and {1}!".format(value1, value2)
-        self.assertEqual(__, string)
+        self.assertEqual("The values are DOH, doh, doh and DOH!", string)
 
     def test_any_python_expression_may_be_interpolated(self):
+        # Python has a set of built-in math functions, including an extensive math module, that allows you to perform mathematical tasks on numbers.
         import math # import a standard python module with math functions
 
         decimal_places = 4
         string = "The square root of 5 is {0:.{1}f}".format(math.sqrt(5),
             decimal_places)
-        self.assertEqual(__, string)
+        self.assertEqual("The square root of 5 is 2.2361", string)
 
     def test_you_can_get_a_substring_from_a_string(self):
+        # string[start: end: step]
+        # Starts at the index indicated and ends at the index indicated.
         string = "Bacon, lettuce and tomato"
-        self.assertEqual(__, string[7:10])
+        self.assertEqual("let", string[7:10])
 
     def test_you_can_get_a_single_character_from_a_string(self):
         string = "Bacon, lettuce and tomato"
-        self.assertEqual(__, string[1])
+        self.assertEqual("a", string[1])
 
     def test_single_characters_can_be_represented_by_integers(self):
-        self.assertEqual(__, ord('a'))
-        self.assertEqual(__, ord('b') == (ord('a') + 1))
+        # The ord() function returns the number representing the unicode code of a specified character.
+        self.assertEqual(97, ord('a'))
+        self.assertEqual(True, ord('b') == (ord('a') + 1))
 
     def test_strings_can_be_split(self):
+        # The split() method splits a string into a list. You can specify the separator, default separator is any whitespace.
         string = "Sausage Egg Cheese"
         words = string.split()
-        self.assertListEqual([__, __, __], words)
+        self.assertListEqual(['Sausage', 'Egg', 'Cheese'], words)
 
     def test_strings_can_be_split_with_different_patterns(self):
+        # A RegEx, or Regular Expression, is a sequence of characters that forms a search pattern.
+        # RegEx can be used to check if a string contains the specified search pattern.
+        # Python has a built-in package called re, which can be used to work with Regular Expressions.
         import re #import python regular expression library
 
         string = "the,rain;in,spain"
@@ -50,25 +61,26 @@ class AboutStringManipulation(Koan):
 
         words = pattern.split(string)
 
-        self.assertListEqual([__, __, __, __], words)
+        self.assertListEqual(['the', 'rain', 'in', 'spain'], words)
 
         # Pattern is a Python regular expression pattern which matches ',' or ';'
 
     def test_raw_strings_do_not_interpret_escape_characters(self):
         string = r'\n'
         self.assertNotEqual('\n', string)
-        self.assertEqual(__, string)
-        self.assertEqual(__, len(string))
+        self.assertEqual('\\n', string)
+        self.assertEqual(2, len(string))
 
         # Useful in regular expressions, file paths, URLs, etc.
 
     def test_strings_can_be_joined(self):
+        # The join() method takes all items in an iterable and joins them into one string.
         words = ["Now", "is", "the", "time"]
-        self.assertEqual(__, ' '.join(words))
+        self.assertEqual("Now is the time", ' '.join(words))
 
     def test_strings_can_change_case(self):
-        self.assertEqual(__, 'guido'.capitalize())
-        self.assertEqual(__, 'guido'.upper())
-        self.assertEqual(__, 'TimBot'.lower())
-        self.assertEqual(__, 'guido van rossum'.title())
-        self.assertEqual(__, 'ToTaLlY aWeSoMe'.swapcase())
+        self.assertEqual("Guido", 'guido'.capitalize())
+        self.assertEqual("GUIDO", 'guido'.upper())
+        self.assertEqual("timbot", 'TimBot'.lower())
+        self.assertEqual("Guido Van Rossum", 'guido van rossum'.title())
+        self.assertEqual("tOtAlLy AwEsOmE", 'ToTaLlY aWeSoMe'.swapcase())
--- koans/about_strings.py
+++ koans/about_strings.py
@@ -6,89 +6,104 @@ from runner.koan import *
 class AboutStrings(Koan):
 
     def test_double_quoted_strings_are_strings(self):
+        # Strings in python are surrounded by either single quotation marks, or double quotation marks.
         string = "Hello, world."
-        self.assertEqual(__, isinstance(string, str))
+        self.assertEqual(True, isinstance(string, str))
 
     def test_single_quoted_strings_are_also_strings(self):
+        # Strings in python are surrounded by either single quotation marks, or double quotation marks.
         string = 'Goodbye, world.'
-        self.assertEqual(__, isinstance(string, str))
+        self.assertEqual(True, isinstance(string, str))
 
     def test_triple_quote_strings_are_also_strings(self):
+        # You can assign a multiline string to a variable by using three quotes
         string = """Howdy, world!"""
-        self.assertEqual(__, isinstance(string, str))
+        self.assertEqual(True, isinstance(string, str))
 
     def test_triple_single_quotes_work_too(self):
+        # You can assign a multiline string to a variable by using three quotes
         string = '''Bonjour tout le monde!'''
-        self.assertEqual(__, isinstance(string, str))
+        self.assertEqual(True, isinstance(string, str))
 
     def test_raw_strings_are_also_strings(self):
+        # Python raw string is created by prefixing a string literal with ‘r’ or ‘R’.
+        # Python raw string treats backslash (\) as a literal character.
+        # This is useful when we want to have a string that contains backslash and don’t want it to be
+        # treated as an escape character.
         string = r"Konnichi wa, world!"
-        self.assertEqual(__, isinstance(string, str))
+        self.assertEqual(True, isinstance(string, str))
 
     def test_use_single_quotes_to_create_string_with_double_quotes(self):
+        # we can use an escape sequence to insert a doubel quote.
         string = 'He said, "Go Away."'
-        self.assertEqual(__, string)
+        self.assertEqual('He said, \"Go Away.\"', string)
 
     def test_use_double_quotes_to_create_strings_with_single_quotes(self):
+        # We can use an escape sequence on a single quote.
         string = "Don't"
-        self.assertEqual(__, string)
+        self.assertEqual("Don\'t", string)
 
     def test_use_backslash_for_escaping_quotes_in_strings(self):
+        # This two are the same
         a = "He said, \"Don't\""
         b = 'He said, "Don\'t"'
-        self.assertEqual(__, (a == b))
+        self.assertEqual(True, (a == b))
 
     def test_use_backslash_at_the_end_of_a_line_to_continue_onto_the_next_line(self):
+        # The len() function returns the number of items in an object or string.
         string = "It was the best of times,\n\
-It was the worst of times."
-        self.assertEqual(__, len(string))
+        It was the worst of times."
+        self.assertEqual(60, len(string))
 
     def test_triple_quoted_strings_can_span_lines(self):
         string = """
 Howdy,
 world!
 """
-        self.assertEqual(__, len(string))
+        # The length of the words plus the lines
+        self.assertEqual(15, len(string))
 
     def test_triple_quoted_strings_need_less_escaping(self):
+        # These are both the same
         a = "Hello \"world\"."
         b = """Hello "world"."""
-        self.assertEqual(__, (a == b))
+        self.assertEqual(True, (a == b))
 
     def test_escaping_quotes_at_the_end_of_triple_quoted_string(self):
         string = """Hello "world\""""
-        self.assertEqual(__, string)
+        self.assertEqual('Hello "world"', string)
 
     def test_plus_concatenates_strings(self):
         string = "Hello, " + "world"
-        self.assertEqual(__, string)
+        self.assertEqual("Hello, world", string)
 
     def test_adjacent_strings_are_concatenated_automatically(self):
         string = "Hello" ", " "world"
-        self.assertEqual(__, string)
+        self.assertEqual("Hello, world", string)
 
     def test_plus_will_not_modify_original_strings(self):
         hi = "Hello, "
         there = "world"
         string = hi + there
-        self.assertEqual(__, hi)
-        self.assertEqual(__, there)
+        self.assertEqual("Hello, ", hi)
+        self.assertEqual("world", there)
 
     def test_plus_equals_will_append_to_end_of_string(self):
         hi = "Hello, "
         there = "world"
         hi += there
-        self.assertEqual(__, hi)
+        self.assertEqual("Hello, world", hi)
 
     def test_plus_equals_also_leaves_original_string_unmodified(self):
         original = "Hello, "
         hi = original
         there = "world"
         hi += there
-        self.assertEqual(__, original)
+        self.assertEqual("Hello, ", original)
 
     def test_most_strings_interpret_escape_characters(self):
+        # The escape character is not counted in the length It tells the parser that the character following is printable, not a token.
         string = "\n"
         self.assertEqual('\n', string)
         self.assertEqual("""\n""", string)
-        self.assertEqual(__, len(string))
+        self.assertEqual(1, len(string))
